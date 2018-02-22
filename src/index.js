dd45b4ef530f7413ad34e42a776e0303edcb288a
nonoike <ij4nu8d4fw@gmail.com>
2018-02-22 20:32:07 +0900

    add ava and parallels testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const test = require('ava')
+
 function sum (...nums) {
   return nums.reduce((a, b) => a + b)
 }
 
-console.log(
+test('sum of 1, 2, 3, 4 must be 10', () => {
   sum(1, 2, 3, 4)
-)
+})
