739f7cda8d346b0c176ee6f35edc420723e9dff9
nonoike <ij4nu8d4fw@gmail.com>
2018-02-22 21:07:44 +0900

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
+const ALLOW = 0
+const WARN = 1
+const ERROR = 2
+
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "globals": {
+        ga: true,
+        '$': true
+    },
+    // https://eslint.org/docs/rules/
+    "rules": {
+        "no-console": ERROR
+    }
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ function sum(...nums) {
   return nums.reduce((a, b) => a + b);
 }
 
+// a();
+// ga();
+
 test('sum of 1, 2, 3, 4 must be 10', (t) => {
+  // console.log('sum(1, 2, 3, 4');
   t.is(sum(1, 2, 3, 4), 10);
 });
