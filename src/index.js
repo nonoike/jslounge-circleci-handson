4dcad48c19e26715eff79cb85d804ba6ffb5d599
nonoike <ij4nu8d4fw@gmail.com>
2018-02-22 20:58:10 +0900

    add airbnb rules and fix

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const test = require('ava')
+const test = require('ava');
 
-function sum (...nums) {
-  return nums.reduce((a, b) => a + b)
+function sum(...nums) {
+  return nums.reduce((a, b) => a + b);
 }
 
 test('sum of 1, 2, 3, 4 must be 10', (t) => {
-  t.is(sum(1, 2, 3, 4), 10)
-})
+  t.is(sum(1, 2, 3, 4), 10);
+});
