1bb69a5d29a6456997afd54abe6863064d49c1b2
nonoike <ij4nu8d4fw@gmail.com>
2018-02-22 20:38:34 +0900

    fix test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ function sum (...nums) {
   return nums.reduce((a, b) => a + b)
 }
 
-test('sum of 1, 2, 3, 4 must be 10', () => {
-  sum(1, 2, 3, 4)
+test('sum of 1, 2, 3, 4 must be 10', (t) => {
+  t.is(sum(1, 2, 3, 4), 10)
 })
